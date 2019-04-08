6598f085a98472222438e9f85c36f1b0df81d4f3
bora <borarooney@gmail.com>
2019-04-08 22:03:00 +0200

    half footer :)

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -11,13 +11,10 @@
 
 <footer class="footer-container">
 	<div class="footer-grid">
-		<?php dynamic_sidebar( 'footer-widgets' ); ?>
+		<h3 class= "footer-title">Interestend in making something great?</h3>
+		<a href="#" class="footer-link">Give us a call or contact us!</a>
 	</div>
 </footer>
-
-<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-	</div><!-- Close off-canvas content -->
-<?php endif; ?>
 
 <?php wp_footer(); ?>
 </body>
