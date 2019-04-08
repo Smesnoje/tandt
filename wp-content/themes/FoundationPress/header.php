2111227fd0965269b93f9c2f33b4d16bdf7b9a30
bora <borarooney@gmail.com>
2019-04-08 20:39:16 +0200

    front without footer

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -15,6 +15,9 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link rel="stylesheet" href="http://localhost:3000/tandt/wp-content/themes/FoundationPress/node_modules/aos/dist/aos.css" />
+		<script src="http://localhost:3000/tandt/wp-content/themes/FoundationPress/node_modules/aos/dist/aos.js"></script>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
--- wp-content/themes/FoundationPress/page-templates/front-archive.php
+++ wp-content/themes/FoundationPress/page-templates/front-archive.php
@@ -4,35 +4,58 @@ Template Name: front-archive
 */
 get_header(); ?>
 
-<header>
+<!-- <div class="main-container"> -->
+
+  <script>
+    AOS.init();
+  </script>
+
+<div class="front-header main-container">
 	
-<h1>Our Work</h1>
-<div class="filter filter_tags"></div>
-</header>
-<?php $posts = get_posts(-1) ?>
-<?php foreach($posts as $post):?>
+	<h2 class="front-header_title">Our Work</h2>
+
+	<div class="front-header_filters front-header_filters--tags">
+		<span class="front-header_filters--tag">branding</span>
+		<span class="front-header_filters--tag">packaging</span>
+		<span class="front-header_filters--tag">tvc</span>
+		<span class="front-header_filters--tag">soical</span>
+		<span class="front-header_filters--tag">web design</span>
+		<span class="front-header_filters--tag">category</span>
+	</div>
+</div>
+<div class="posts-container">
+
+	<?php $posts = get_posts(-1) ?>
+	<?php foreach($posts as $post):?>
 	<?php // echo '<pre>' , var_dump($post) , '</pre>';?>
 	<!-- gets post tags -->
 	<?php $post_tags = get_field( "tags" );?>
-	<div class="post_card <?php echo $post_tags;?>" style="background-image: url(<?php echo(get_the_post_thumbnail_url());?>)">
-	<!-- displays post title -->
-	<h3 class="post_card_heading"><?php echo($post->post_title);?></h3>
-	<!-- gets post featured image -->
-	<!-- <?php echo(get_the_post_thumbnail_url());?> -->
+		<div data-aos="fade-up-right" class="post-card <?php echo $post_tags;?>" style="background-image: url(<?php echo(get_the_post_thumbnail_url());?>)">
+		<!-- displays post title -->
+			<div class="post-card_hover">
+				<div class="post-card_title-tag-wrap">
+					<h3 class="post-card_title"><?php echo($post->post_title);?></h3>
+					<!-- gets post featured image -->
+					<!-- <?php echo(get_the_post_thumbnail_url());?> -->
 	
-	<!-- array of tags -->
-	<?php $post_tags_array = explode(" ",$post_tags);?>
-	<!-- displays post tags if not empty-->
-	<?php if($post_tags!=''):?>
-	<?php foreach($post_tags_array as $tag):?>
-		<a class="post_tag" href="#"><?php echo $tag?></a>
+					<!-- array of tags -->
+					<?php $post_tags_array = explode(" ",$post_tags);?>
+
+					<!-- displays post tags if not empty-->
+					<?php if($post_tags!=''):?>
+						<?php foreach($post_tags_array as $tag):?>
+							<a class="post-card_tag" href="#"><?php echo $tag?></a>
+						<?php endforeach;?>
+					<?php endif;?>
+				</div>
+			</div>
+		</div>
 	<?php endforeach;?>
-	<?php endif;?>
-	</div>
-<?php endforeach;?>
+</div>
 
 
 
 
 <script type="text/javascript" src="<?php echo $theme_root;?>/js/front-archive.js"></script>
+<!-- </div> -->
 <?php get_footer();
