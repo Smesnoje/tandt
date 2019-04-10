ff5cf25674e6e35feb0e96b3197d922405975bd9
Smesnoje <anru1996@gmail.com>
2019-04-10 17:40:56 +0200

    section order, links and small fixes myworkhereisdone

--- wp-content/themes/FoundationPress/page-templates/sections/post-video-section.php
+++ wp-content/themes/FoundationPress/page-templates/sections/post-video-section.php
@@ -9,6 +9,23 @@
 			</div>
 		<?php endif; ?>
 
+		<!-- <?php if( get_field('mp4_video') ): ?>
+			<div class="post-video-content">
+			<video width="400" controls>
+			<source src="<?php echo (get_field('mp4_video')['url']);?>" type="video/mp4">
+			</video>
+				
+			</div>
+		<?php endif; ?>
+		<?php if( get_field('webm_video') ): ?>
+			<div class="post-video-content">
+			<video width="400" controls>
+			<source src="<?php echo (get_field('webm_video')['url']);?>" type="video/webm">
+			</video>
+
+			</div>
+		<?php endif; ?> -->
+
 	</div>
 
 </section>
--- wp-content/themes/FoundationPress/single-post.php
+++ wp-content/themes/FoundationPress/single-post.php
@@ -9,39 +9,50 @@
 get_header(); ?>
 
 
-<?php 
-// $sections_array=[];
+<?php   
+$array_of_templates = array(
+  "post-hero-section"=>"",
+  "post-subtext-section"=>"", 
+  "post-img-section"=>"",
+  "post-text-grid-section"=>"",
+  "post-video-section"=>"", 
+  "post-card-image-section"=>"",
+  "post-asymmetric-text-grid-section"=>"",
+  "post-above-footer-section"=>"", 
+);
 
-   
-//     $fields = get_field_objects();
+  $order = explode(' ',get_field('sections_order'));
+  $order = preg_split('/\s+/', get_field('sections_order'));
+  if(count($order)!=count($array_of_templates)){
+    $order = array_fill(0, count($array_of_templates), '0');
+  
+  }
+$order_counter= 0;
+$new_array =[];
+foreach($array_of_templates as $key=>$value){
+  $value = $order[$order_counter];
+  $new_array[$key] = $value;
+  $order_counter++; 
+}
+asort($new_array);
 
-//       foreach( $fields as $name => $value ){
-        
-//         array_push($sections_array,$name);
-       
-//       }
-    //   var_dump($sections_array);
-                   
-  // var_dump(get_post());
-  
-  include('page-templates/sections/post-hero-section.php');
-  include('page-templates/sections/post-subtext-section.php');
-  include('page-templates/sections/post-img-section.php');
-  include('page-templates/sections/post-text-grid-section.php');
-  include('page-templates/sections/post-video-section.php');
-  include('page-templates/sections/post-card-image-section.php');
-  include('page-templates/sections/post-asymmetric-text-grid-section.php');
-  include('page-templates/sections/post-above-footer-section.php');
-
-  
-  
+foreach($new_array as $template_name=>$order){
+  include('page-templates/sections/'.$template_name.'.php');
+}  
 ?>
+<div class="post_navigation"> 
+<a href="<?php echo get_home_url();?>">See all projects</a>
+<?php if (get_previous_post()):?>
+  <!-- <a href="<?php echo (get_permalink(get_previous_post())); ?>">Previous project</a> -->
+<?php endif;?>
+<?php if (get_next_post()):?>
+  <a href="<?php echo ((get_permalink(get_next_post()))); ?>">Next project</a>
+<?php endif;?>
+</div>
 <div class="main-container">
 	<div class="main-grid">
 		<main class="main-content">
 
-
-			
 		</main>
 	</div>
 </div>
